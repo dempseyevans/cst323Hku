e34df4eee90fd8b13c4481523d3c522b8297a0a5
dempseyevans <124719789+dempseyevans@users.noreply.github.com>
2024-08-04 22:32:20 -0400

    Update userService.java

--- src/main/java/com/cst323/app/service/userService.java
+++ src/main/java/com/cst323/app/service/userService.java
@@ -2,6 +2,8 @@ package com.cst323.app.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,29 +16,35 @@ public class userService {
     @Autowired
     private userRepository userRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(userService.class);
+
     public List<user> getAllUsers()
     {
+        logger.info("Finding Users");
         return userRepository.findAll();
     }
 
     public user saveUser(user user)
     {
+        logger.info("Users Saved");
         return userRepository.save(user);
     }
     
     public user updateUser(user user)
     {
+        logger.info("User Updated");
         return userRepository.save(user);
     }
 
     public void deleteUser(Long id)
     {
+        logger.info("User Deleted");
         userRepository.deleteById(id);
     }
 
     public user getUserById(Long id) {
         Optional<user> findUser = userRepository.findById(id);
-
+        logger.info("Finding User");
         return findUser.orElse(null);
     }
     
